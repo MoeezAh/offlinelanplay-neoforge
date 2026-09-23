3a26fb121f22be6654f0b3710d19766a5af37c23
MoeezAh <atif4ever@hotmail.com>
2026-09-23 10:43:46 +0500

    Merge pull request #2 from MoeezAh/multi-loader-minecraft-mod-setup-55dae
    Update from task 51cb9bc0-e35e-453c-8f7f-9a21c4555dae

---
+++ fabric/src/main/java/com/github/moeezah/OfflineLanPlay.java
@@ -0,0 +1,20 @@
+package com.github.moeezah;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.GameType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class OfflineLanPlay implements ModInitializer {
+    public static final String MODID = "offlinelanplay";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Offline LAN Play initialized");
+    }
+}
